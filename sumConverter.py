521afb25c78fa7859507dc4c76c1f9e1d10b4107
malaki-mind <155403448+malaki-mind@users.noreply.github.com>
2024-01-24 01:04:39 -0700

    added sumConverter
    simple exercise in list comprehension. I show my initial solution and then I provide my most optimal solution.

---
+++ sumConverter.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 24 00:00:33 2024
+
+@author: Max McMahon
+
+solution to quiz prompt in Ardit Sulce's Python Mega Course on Udemy
+"""
+
+#before truly understanding List Comprehension
+"""
+def sumConverter(array):
+    float_true = [float(i) for i in array]
+    print(float_true)
+    i = 0
+    newSum = 0
+    while i < len(float_true):
+        newSum += float(float_true[i])
+        i+=1
+    return newSum
+    
+nums = ['1.2','7.2','4.5']
+sumConverter(nums)
+"""
+
+#after reading a bit more about List Comprehensions
+def sumConverter(array):
+    newSum = 0
+    newSum = sum(float(i) + newSum for i in array)
+    return newSum
+
+nums = ['1.2','7.2','4.5']
+sumConverter(nums)
